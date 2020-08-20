9cb16dc4351fb71246a94f51e84907f64234f67f
shekabhi369 <shekabhi369@gmail.com>
2020-08-20 18:33:43 +0530

    Updates

---
+++ precache-manifest.428e7b945c47f2d916153fd51e41c8e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b70cdeb7092495f200e221bbff783b",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "3e701ce92abf17fa2868",
+    "url": "/myportfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9151159b9ad659ee2fbb",
+    "url": "/myportfolio/static/css/main.77e0cda7.chunk.css"
+  },
+  {
+    "revision": "3e701ce92abf17fa2868",
+    "url": "/myportfolio/static/js/2.d72ee980.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/myportfolio/static/js/2.d72ee980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9151159b9ad659ee2fbb",
+    "url": "/myportfolio/static/js/main.b2ef1b18.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  }
+]);
